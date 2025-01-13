67be556b8d8cf87f6a246d784e759a6ac3ee8c0a
RohanKhatua <contact@rohankhatua.dev>
2025-01-13 14:42:59 +0530

    chore: registering routes, adding packages and cleanup

--- main.go
+++ main.go
@@ -1,15 +1,15 @@
 package main
 
 import (
+	cl "go-weather/custom-logger"
 	"go-weather/routes"
+	"go-weather/routes/admin"
 	"go-weather/utils"
 
 	"github.com/gin-gonic/gin"
 )
 
-func main() {
-	// Create the InfluxDB client and initialize the API objects
-	utils.CreateInfluxClient()
+func setupRouter() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
@@ -17,8 +17,28 @@ func main() {
 		})
 	})
 
-	r.GET("/sensor-data", routes.SaveSensorDataHandler)
+	// Admin routes
+	r.GET("/admin/view-data", admin.ViewAllDataHandler)
+	r.DELETE("/admin/delete-data", admin.DeleteDataHandler)
 
-	r.Run(":8080")
+	// Sensor data route
+	r.POST("/sensor-data", routes.SaveSensorDataHandler)
+	r.GET("/sensor-data", routes.RetrieveDataByTimeDuration)
+
+	// Run the server
+	err := r.Run(":8080")
+	if err != nil {
+		cl.Logger.Fatal(err)
+	}
+
+}
+
+func main() {
+	// Initialize the logger
+	cl.Init()
+
+	// Create the InfluxDB client and initialize the API objects
+	utils.CreateInfluxClient()
+	setupRouter()
 
 }
